aa159d747ba799fb039b5a7ac7bb0c5fcb817fa3
Phạm Quốc Hưng <165866405+LeviathanST@users.noreply.github.com>
2024-09-10 22:53:42 +0700

    add: global definition setting, document

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,19 @@
 using Controller;
 using Model;
-using Service;
-using Utils;
 
 namespace app;
 
 static class Program
 {
+
     // Entry point
     [STAThread]
     static void Main()
     {
+
+        SettingController sc = new SettingController();
+        DocumentController dc = new DocumentController(sc.s);
+
         System.Console.WriteLine("Hello World!");
     }    
 }
